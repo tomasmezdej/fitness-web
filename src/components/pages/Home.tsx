41b3c34b697d86125a25cc6fcdb24030a3c40ddf
Tomáš Meždej <tmezdej@gmail.com>
2023-03-20 17:16:30 +0100

    typescript init

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,13 @@
 import Accordion from 'react-bootstrap/Accordion';
 
 const Home = () => {
-  console.log({ pes: 'ahoj' });
-  const hey = () => {
-    console.log('hej');
-  };
+
   return (
     <>
       <h1>Bootstrap installed</h1>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header te="s">Accordion Item #1</Accordion.Header>
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
             enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
